b47828fd721915402c94a637f847d98d2ee6ab44
Jason Rosen <9jbr99@gmail.com>
2020-01-11 14:45:37 -0500

    initial

---
+++ inc/sendEmail.php
@@ -0,0 +1,17 @@
+﻿<?php
+$sender = '9jbr99@gmail.com';
+$recipient = 'ericcolesmith@gmail.com';
+
+$subject = "php mail test";
+$message = "php test message";    
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
